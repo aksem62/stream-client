836930c6d2e6a49d941db71cbed077a522457eb0
aksem62 <aksem62@gmail.com>
2020-12-01 17:20:31 +0300

    scaffold routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,26 @@
 import React, { Fragment } from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-    <Fragment>
-      <div>Page One</div>
-      <div>
-        <Link to="/pagetwo">Go to page 2</Link>
-      </div>
-      <Link to="/pagetwo/3">Go to page 3</Link>
-    </Fragment>
-  );
-};
-const PageTwo = () => {
-  return (
-    <Fragment>
-      <div>Page Two</div>
-      <div>
-        <button>Click me!</button>
-      </div>
-      <div>
-        <Link to="/">Go to first page</Link>
-      </div>
-      <Link to="/pagetwo/3">Go to third page</Link>
-    </Fragment>
-  );
-};
-const PageThree = () => {
-  return (
-    <Fragment>
-      <div>Page Three</div>
-      <p>This is third page</p>
-      <div>
-        <Link to="/">Go to first page</Link>
-      </div>
-      <Link to="/pagetwo">Go to second page</Link>
-    </Fragment>
-  );
-};
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" exact component={PageTwo} />
-          <Route path="/pagetwo/3" component={PageThree} />
-        </div>
-      </BrowserRouter>
-    </div>
+    <Fragment>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams/show" exact component={StreamShow} />
+          </div>
+        </BrowserRouter>
+      </div>
+    </Fragment>
   );
 };
 
