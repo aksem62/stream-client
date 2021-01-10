9734b152f4d39db68d221099833d39073357ad94
aksem62 <aksem62@gmail.com>
2021-01-10 23:29:25 +0300

    add redux connect

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import streams from "../apis/streams";
 import { SIGN_IN, SIGN_OUT } from "./types";
 export const signIn = (userId) => {
   return {
@@ -9,4 +10,7 @@ export const signOut = () => {
   return {
     type: SIGN_OUT,
   };
+};
+export const createStream = (formValues) => async (dispatch) => {
+  streams.post("/streams", formValues);
 };
