31ba127f6d57dc58520a7e14251b733e9b5fc4e7
aksem62 <aksem62@gmail.com>
2021-03-23 14:28:08 +0300

    made SreamEdit to fetch data

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,11 +1,25 @@
 import React, { Fragment } from "react";
+import { fetchStream } from "../../actions";
+import { connect } from "react-redux";
 
-const StreamEdit = () => {
-  return (
-    <Fragment>
-      <div>StreamEdit</div>
-    </Fragment>
-  );
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    console.log(this.props);
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <>
+        <div>{this.props.stream.title}</div>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
-
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
